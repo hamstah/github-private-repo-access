38f3e5815f49f898bf5dccf7b1733f95ff310e7f
Nico Esteves <git@hamstah.com>
2021-12-29 15:36:58 +0100

    Support base64 encoded private keys

--- jwt.go
+++ jwt.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"crypto/rsa"
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -17,16 +19,25 @@ type JWTHelper struct {
 func NewJWTHelper(key string) (*JWTHelper, error) {
 	var keyBytes []byte
 	file, err := os.Open(key)
-	if os.IsNotExist(err) {
-		keyBytes = []byte(key)
-	} else {
+
+	if err == nil {
 		defer file.Close()
 
 		keyBytes, err = ioutil.ReadAll(file)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("can't read key file: %w", err)
+		}
+	} else {
+		if strings.HasPrefix(key, "LS0t") {
+			keyBytes, err = base64.StdEncoding.DecodeString(key)
+			if err != nil {
+				return nil, fmt.Errorf("can't decode base64 string: %w", err)
+			}
+		} else {
+			keyBytes = []byte(key)
 		}
 	}
+
 	rsaKey, err := jwt.ParseRSAPrivateKeyFromPEM(keyBytes)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse private key: %w", err)
